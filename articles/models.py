39c906c6f4f4ce4f8e7cf785c6e9b1736f932e5b
meron <merronmuche@gmail.com>
2022-11-06 13:53:12 -0400

    add data in the model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class article(models.Model):
+    title= models.CharField(max_length=200)
+    content=models.CharField(max_length=250)
+    
